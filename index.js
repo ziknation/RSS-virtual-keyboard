420a3565ad1f0dfa0e19c4cbeedd6c8ddb457216
Yuri Zikeev <zikeev.yura@gmail.com>
2019-11-03 20:36:07 +0300

    feat: lang change

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ createTextarea();
 createRussianKeys();
 document.addEventListener('keydown', keyDown);
 document.addEventListener('keyup', keyUp);
+runOnKeys(langChange, 'Shift', 'Control');
+let russian = true;
+let caps = false;
+let focus = false;
 
 function createRussianKeys(){
   let keys = [
@@ -42,18 +46,67 @@ function createRussianKeys(){
   document.body.append(table);
 }
 
+function createEnglishKeys(){
+  let keys = [
+    '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
+    'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Delete',
+    'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter',
+    'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '\u2191',
+    'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '\u2190', '\u2193', '\u2192'
+  ];
+
+  let table = document.createElement('table');
+  table.classList.add('table');
+  const breaks = ['Backspace','Delete','Enter','\u2191','\u2192'];
+
+  let tr = document.createElement('tr');
+  tr.classList.add('tr');
+  keys.forEach(function(k, j){
+    let td = document.createElement('td');
+    td.append(k);
+    td.classList.add('td');
+    tr.append(td);
+    if (k.length > 1){
+      td.classList.add('wide');
+      td.classList.add(k);
+    }
+    if (k == 'Space'){
+      td.classList.remove('wide');
+      td.classList.add('space');
+    }
+    for (let i = 0; i < breaks.length; i++){
+      if (k == breaks[i]){
+        table.append(tr);
+        tr = document.createElement('tr');
+        tr.classList.add('tr');
+      }
+    }
+  });
+  document.body.append(table);
+}
+
 function createTextarea(){
   let textarea = document.createElement('textarea');
   textarea.classList.add('textarea');
+  textarea.addEventListener('focus',focusCheck); 
+  textarea.addEventListener('focusout',focusCheck);
   document.body.append(textarea);
 }
 
 function keyDown(event){
-  keyBacklight(event);
+  if (event.key == 'CapsLock'){
+    capsChanges();
+  }
+  else{
+    keyBacklight(event);
+    writing(event);
+  }
 }
 
 function keyUp(event){
-  removeActiveClass(event);
+  if (event.key != 'CapsLock'){
+    removeActiveClass(event);
+  }
 }
 
 function keyBacklight(event){
@@ -177,5 +230,107 @@ function removeActiveClass(event){
         tds[i].classList.remove('active');
       }
     }
+  }
+}
+
+function runOnKeys(func, ...keys) {
+  let pressed = new Set();
+
+  document.addEventListener('keydown', function(event) {
+    pressed.add(event.key);
+
+    for (let key of keys) {
+      if (!pressed.has(key)) {
+        return;
+      }
+    }
+
+    pressed.clear();
+
+    func();
+  });
+
+  document.addEventListener('keyup', function(event) {
+    pressed.delete(event.key);
+  });
+
+}
+
+function removeTable(){
+  document.querySelector('table').outerHTML = '';
+
+}
+
+function langChange(){
+  removeTable();
+  if (russian){
+    russian = false;
+    createEnglishKeys();
+  }
+  else{
+    russian = true;
+    createRussianKeys();
+  }
+  capsCheck();
+}
+
+function writing(event){
+  if (!focus){
+    let textarea = document.querySelector('textarea');
+    let tds = document.querySelectorAll('td');
+
+    for (let i = 0; i < tds.length; i++){
+      if (tds[i].innerHTML == event.key){
+        if (event.key.length == 1){
+          textarea.value += event.key;
+        }
+        else if (event.key == 'Tab'){
+          textarea.value += '    ';
+        }
+      }
+    }
+  }
+}
+
+function capsChanges(){
+  if (caps){
+    caps = false;
+    let tds = document.querySelectorAll('td');
+    for (let i = 0; i < tds.length; i++){
+      if(tds[i].innerHTML.length == 1){
+        tds[i].innerHTML = tds[i].innerHTML.toLowerCase();
+      }
+    }
+    document.querySelector('.CapsLock').classList.remove('active');
+  }
+  else{
+    caps = true;
+    let tds = document.querySelectorAll('td');
+    for (let i = 0; i < tds.length; i++){
+      if(tds[i].innerHTML.length == 1){
+        tds[i].innerHTML = tds[i].innerHTML.toUpperCase();
+      }
+    }
+    document.querySelector('.CapsLock').classList.add('active');
+  }
+}
+
+function capsCheck(){
+  if (caps){
+    let tds = document.querySelectorAll('td');
+    for (let i = 0; i < tds.length; i++){
+      if(tds[i].innerHTML.length == 1){
+        tds[i].innerHTML = tds[i].innerHTML.toUpperCase();
+      }
+    }
+  }
+}
+
+function focusCheck(){
+  if (focus){
+    focus = false;
+  }
+  else{
+    focus = true;
   }
 }
